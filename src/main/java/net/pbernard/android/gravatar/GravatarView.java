c052bb386a5c4ee8ee7ca20a2ab8d91a17f6aa09
Philippe Bernard <philippe.bernard@gmail.com>
2011-06-20 17:44:00 +0200

    Project backbone: Eclipse project, Maven POM file

---
+++ src/main/java/net/pbernard/android/gravatar/GravatarView.java
@@ -0,0 +1,13 @@
+package net.pbernard.android.gravatar;
+
+import android.content.Context;
+import android.widget.ImageView;
+
+public class GravatarView extends ImageView {
+
+	public GravatarView(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+}
